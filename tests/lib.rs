f7451c80248ec80c8c48ef5d8db9556c2ab48182
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-09-26 10:14:08 -0700

    Refactoring

--- tests/lib.rs
+++ tests/lib.rs
@@ -80,7 +80,7 @@ fn end_group_callable() {
 #[wasm_bindgen_test]
 fn group_callable() {
     let name = &"name".into();
-    let clo = Closure::<dyn Fn()>::new(move || {});
+    let clo = Closure::<dyn Fn()>::new(|| {});
     group(name, clo.as_ref().unchecked_ref());
     clo.forget();
 }
