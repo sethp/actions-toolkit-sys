ee998efcf07ae330f30c544437f93f537f2a768e
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-09-26 09:48:02 -0700

    Use literals for tests
    [skip ci]

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,71 +4,71 @@ use wasm_bindgen_test::*;
 
 // #[wasm_bindgen_test]
 // fn export_secret_callable() {
-//     let name = <&str as Default>::default().into();
-//     let value = <&str as Default>::default().into();
+//     let name = "name".into();
+//     let value = "value".into();
 //     export_secret(name, value);
 // }
 
 #[wasm_bindgen_test]
 fn export_variable_callable() {
-    let name = <&str as Default>::default().into();
-    let value = <&str as Default>::default().into();
+    let name = "name".into();
+    let value = "value".into();
     export_variable(name, value);
 }
 
 #[wasm_bindgen_test]
 fn add_path_callable() {
-    let path = <&str as Default>::default().into();
+    let path = "path".into();
     add_path(path);
 }
 
 #[wasm_bindgen_test]
 fn get_input_callable() {
-    let name = <&str as Default>::default().into();
+    let name = "name".into();
     let options = Default::default();
     get_input(name, options);
 }
 
 #[wasm_bindgen_test]
 fn set_output_callable() {
-    let name = <&str as Default>::default().into();
-    let value = <&str as Default>::default().into();
+    let name = "name".into();
+    let value = "value".into();
     set_output(name, value);
 }
 
 // #[wasm_bindgen_test]
 // fn set_failed_callable() {
-//     let message = <&str as Default>::default().into();
+//     let message = "message".into();
 //     set_failed(message);
 // }
 
 #[wasm_bindgen_test]
 fn debug_callable() {
-    let message = <&str as Default>::default().into();
+    let message = "message".into();
     info(message);
 }
 
 #[wasm_bindgen_test]
 fn error_callable() {
-    let message = <&str as Default>::default().into();
+    let message = "message".into();
     info(message);
 }
 
 #[wasm_bindgen_test]
 fn warning_callable() {
-    let message = <&str as Default>::default().into();
+    let message = "message".into();
     info(message);
 }
 
 #[wasm_bindgen_test]
 fn info_callable() {
-    let message = <&str as Default>::default().into();
+    let message = "message".into();
     info(message);
 }
 
 #[wasm_bindgen_test]
 fn start_group_callable() {
-    let name = <&str as Default>::default().into();
+    let name = "name".into();
     start_group(name);
 }
 
@@ -79,7 +79,7 @@ fn end_group_callable() {
 
 #[wasm_bindgen_test]
 fn group_callable() {
-    let name = <&str as Default>::default().into();
+    let name = "name".into();
     let clo = Closure::<dyn Fn()>::new(move || {});
     group(name, clo.as_ref().unchecked_ref());
     clo.forget();
