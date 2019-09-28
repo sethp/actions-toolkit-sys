8d51c98a7d29f17d64ea94e46fd81b2e9ebef327
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-09-28 12:20:51 -0700

    Move current bindings under core module

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,4 +1,4 @@
-use actions_toolkit_sys::*;
+use actions_toolkit_sys::core;
 use wasm_bindgen::{prelude::*, JsCast};
 use wasm_bindgen_test::*;
 
@@ -6,81 +6,81 @@ use wasm_bindgen_test::*;
 // fn export_secret_callable() {
 //     let name = &"name".into();
 //     let value = &"value".into();
-//     export_secret(name, value);
+//     core::export_secret(name, value);
 // }
 
 #[wasm_bindgen_test]
 fn export_variable_callable() {
     let name = &"name".into();
     let value = &"value".into();
-    export_variable(name, value);
+    core::export_variable(name, value);
 }
 
 #[wasm_bindgen_test]
 fn add_path_callable() {
     let path = &"path".into();
-    add_path(path);
+    core::add_path(path);
 }
 
 #[wasm_bindgen_test]
 fn get_input_callable() {
     let name = &"name".into();
     let options = Default::default();
-    get_input(name, options);
+    core::get_input(name, options);
 }
 
 #[wasm_bindgen_test]
 fn set_output_callable() {
     let name = &"name".into();
     let value = &"value".into();
-    set_output(name, value);
+    core::set_output(name, value);
 }
 
 // #[wasm_bindgen_test]
 // fn set_failed_callable() {
 //     let message = "message".into();
-//     set_failed(message);
+//     core::set_failed(message);
 // }
 
 #[wasm_bindgen_test]
 fn debug_callable() {
     let message = &"message".into();
-    info(message);
+    core::info(message);
 }
 
 #[wasm_bindgen_test]
 fn error_callable() {
     let message = &"message".into();
-    info(message);
+    core::info(message);
 }
 
 #[wasm_bindgen_test]
 fn warning_callable() {
     let message = &"message".into();
-    info(message);
+    core::info(message);
 }
 
 #[wasm_bindgen_test]
 fn info_callable() {
     let message = &"message".into();
-    info(message);
+    core::info(message);
 }
 
 #[wasm_bindgen_test]
 fn start_group_callable() {
     let name = &"name".into();
-    start_group(name);
+    core::start_group(name);
 }
 
 #[wasm_bindgen_test]
 fn end_group_callable() {
-    end_group();
+    core::end_group();
 }
 
 #[wasm_bindgen_test]
 fn group_callable() {
     let name = &"name".into();
     let clo = Closure::<dyn Fn()>::new(|| {});
-    group(name, clo.as_ref().unchecked_ref());
+    core::group(name, clo.as_ref().unchecked_ref());
     clo.forget();
 }
