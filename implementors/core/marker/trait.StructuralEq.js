ce8d73002855b2370061486587918eab1c13651f
darinmorrison <darinmorrison@users.noreply.github.com>
2019-12-20 01:16:50 +0000

    Automated deployment: Fri Dec 20 01:16:50 UTC 2019 a3df65bfc70cfec2de57b2df54aa5a1ae8142671

---
+++ implementors/core/marker/trait.StructuralEq.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["actions_toolkit_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"actions_toolkit_sys/io/struct.CopyOptions.html\" title=\"struct actions_toolkit_sys::io::CopyOptions\">CopyOptions</a>",synthetic:false,types:["actions_toolkit_sys::io::CopyOptions"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"actions_toolkit_sys/io/struct.MoveOptions.html\" title=\"struct actions_toolkit_sys::io::MoveOptions\">MoveOptions</a>",synthetic:false,types:["actions_toolkit_sys::io::MoveOptions"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["actions_toolkit_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"actions_toolkit_sys/io/struct.CopyOptions.html\" title=\"struct actions_toolkit_sys::io::CopyOptions\">CopyOptions</a>",synthetic:false,types:["actions_toolkit_sys::io::CopyOptions"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"struct\" href=\"actions_toolkit_sys/io/struct.MoveOptions.html\" title=\"struct actions_toolkit_sys::io::MoveOptions\">MoveOptions</a>",synthetic:false,types:["actions_toolkit_sys::io::MoveOptions"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
